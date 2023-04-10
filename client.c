8de1185e61ed2092d4bc5cd65eb1c0a74d3c3a6f
moraleseoscar <oestrada.3@gmail.com>
2023-04-09 20:22:03 -0600

    Mensajes globales, maximo de usuarios y salida

--- client.c
+++ client.c
@@ -1,53 +1,131 @@
-#include <arpa/inet.h>
 #include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
-int main(int argc, char **argv)
-{
-	if(argc > 2) {
-		int status, valread, client_fd, puerto;
-		char *ip;
-		puerto = atoi(argv[2]);
-		ip = argv[1];
-		struct sockaddr_in serv_addr;
-		char* hello = "Hello from client";
-		char buffer[1024] = { 0 };
-		if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-			printf("\n Socket creation error \n");
-			return -1;
+#define LENGTH 2048
+
+// Global variables
+volatile sig_atomic_t flag = 0;
+int sockfd = 0;
+char name[32];
+
+void str_overwrite_stdout() {
+  printf("%s", "> ");
+  fflush(stdout);
+}
+
+void str_trim_lf (char* arr, int length) {
+  int i;
+  for (i = 0; i < length; i++) { // trim \n
+    if (arr[i] == '\n') {
+      arr[i] = '\0';
+      break;
+    }
+  }
+}
+
+void catch_ctrl_c_and_exit(int sig) {
+    flag = 1;
+}
+
+void send_msg_handler() {
+  char message[LENGTH] = {};
+	char buffer[LENGTH + 32] = {};
+
+  while(1) {
+  	str_overwrite_stdout();
+    fgets(message, LENGTH, stdin);
+    str_trim_lf(message, LENGTH);
+
+    if (strcmp(message, "exit") == 0) {
+			break;
+    } else {
+      sprintf(buffer, "%s: %s\n", name, message);
+      send(sockfd, buffer, strlen(buffer), 0);
+    }
+
+		bzero(message, LENGTH);
+    bzero(buffer, LENGTH + 32);
+  }
+  catch_ctrl_c_and_exit(2);
+}
+
+void recv_msg_handler() {
+	char message[LENGTH] = {};
+  while (1) {
+		int receive = recv(sockfd, message, LENGTH, 0);
+    if (receive > 0) {
+      printf("%s", message);
+      str_overwrite_stdout();
+    } else if (receive == 0) {
+			break;
+    } else {
+			// -1
 		}
+		memset(message, 0, sizeof(message));
+  }
+}
 
-		serv_addr.sin_family = AF_INET;
-		serv_addr.sin_port = htons(puerto);
-
-		// Convert IPv4 and IPv6 addresses from text to binary
-		// form
-		if (inet_pton(AF_INET, ip, &serv_addr.sin_addr)
-			<= 0) {
-			printf(
-				"\nInvalid address/ Address not supported \n");
-			return -1;
-		}
-
-		if ((status
-			= connect(client_fd, (struct sockaddr*)&serv_addr,
-					sizeof(serv_addr)))
-			< 0) {
-			printf("\nConnection Failed \n");
-			return -1;
-		}
-		send(client_fd, hello, strlen(hello), 0);
-		printf("Hello message sent\n");
-		valread = read(client_fd, buffer, 1024);
-		printf("%s\n", buffer);
-
-		// closing the connected socket
-	close(client_fd);
-	} else {
-		printf("Ingrese una ip y un puerto.\n");
+int main(int argc, char **argv){
+	if(argc != 4){
+		printf("Usage: %s <port>\n", argv[0]);
+		return EXIT_FAILURE;
 	}
-	return 0;
+
+	char *name = argv[1];
+	char *ip = argv[2];
+	int port = atoi(argv[3]);
+
+	signal(SIGINT, catch_ctrl_c_and_exit);
+
+	struct sockaddr_in server_addr;
+
+	/* Socket settings */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(ip);
+	server_addr.sin_port = htons(port);
+
+
+	// Connect to Server
+	int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+	if (err == -1) {
+		printf("ERROR: connect\n");
+		return EXIT_FAILURE;
+	}
+
+	// Send name
+	send(sockfd, name, 32, 0);
+
+	printf("=== Bienvenidos al Proyecto Chat ===\n");
+
+	pthread_t send_msg_thread;
+	if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0){
+		printf("ERROR: pthread\n");
+	return EXIT_FAILURE;
+	}
+
+		pthread_t recv_msg_thread;
+	if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0){
+		printf("ERROR: pthread\n");
+		return EXIT_FAILURE;
+	}
+
+	while (1){
+		if(flag){
+			printf("\nChat cerrado\n");
+			break;
+    }
+	}
+
+	close(sockfd);
+
+	return EXIT_SUCCESS;
 }
